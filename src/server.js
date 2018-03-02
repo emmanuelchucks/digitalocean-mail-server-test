1e3be41b31cae613a3dde64971ca665c6c87817d
Emmanuel Chucks <echucks19@gmail.com>
2018-03-02 18:59:03 +0000

    Fixed port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
 app.use(express.static("public"))
-app.listen(8080, function () {
+app.listen(process.env.PORT, function () {
     console.log("Express server is running");
 });
